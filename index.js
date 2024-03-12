fa1d2b09558d7d7a93283ff6d4990b75f711849d
Sandy Fu <86136754+fusandy@users.noreply.github.com>
2024-03-12 22:10:30 +0800

    0312 adjust css and player logic

--- index.js
+++ index.js
@@ -1,60 +1,100 @@
-// 其他音效
 const opening = document.querySelector('#openingButton');
 const ending = document.querySelector('#endingButton');
 const drum = document.querySelector('#drumButton');
 const award = document.querySelector('#awardButton');
 const game1 = document.querySelector('#game1Button');
 const game2 = document.querySelector('#game2Button');
+const game3 = document.querySelector('#game3Button');
 
-const openingMusic = new Audio('media/opening_music.mp3');
-const endingMusic = new Audio('media/ending_music.mp3');
-const drumMusic = new Audio('media/drum_music.mp3');
-const awardMusic = new Audio('media/award_music.mp3');
-const game1Music = new Audio('media/game1_music.mp3');
-const game2Music = new Audio('media/game2_music.mp3');
-
-opening.addEventListener('click', () => {
-  openingMusic.paused ? openingMusic.play() : openingMusic.pause();
-});
-
-ending.addEventListener('click', () => {
-  endingMusic.paused ? endingMusic.play() : endingMusic.pause();
-});
-
-drum.addEventListener('click', () => {
-  drumMusic.paused ? drumMusic.play() : drumMusic.pause();
-});
-
-award.addEventListener('click', () => {
-  awardMusic.paused ? awardMusic.play() : awardMusic.pause();
-});
-
-game1.addEventListener('click', () => {
-  console.log(`game1Music: ${game1Music.paused}`)
-  game1Music.paused ? game1Music.play() : game1Music.pause();
-});
-
-game2.addEventListener('click', () => {
-  game2Music.paused ? game2Music.play() : game2Music.pause();
-});
-
-// 瘋狂黑白猜
 const audio = document.querySelector('#myAudio');
 const speedInput = document.querySelector('#speedInput');
 const resetButton = document.querySelector('#resetButton');
+
+opening.addEventListener('click', (e) => {
+  e.preventDefault();
+  opening.classList.add('animate');
+  audio.src = 'media/opening_music.mp3';
+  audio.play();
+
+  setTimeout(() => {
+    opening.classList.remove('animate')
+  }, 300);
+});
+
+ending.addEventListener('click', (e) => {
+  e.preventDefault();
+  ending.classList.add('animate');
+  audio.src = 'media/ending_music.mp3';
+  audio.play();
+
+  setTimeout(() => {
+    ending.classList.remove('animate')
+  }, 300);
+});
+
+drum.addEventListener('click', (e) => {
+  e.preventDefault();
+  drum.classList.add('animate');
+  audio.src = 'media/drum_music.mp3';
+  audio.play();
+
+  setTimeout(() => {
+    drum.classList.remove('animate')
+  }, 300);
+});
+
+award.addEventListener('click', (e) => {
+  e.preventDefault();
+  award.classList.add('animate');
+  audio.src = 'media/award_music.mp3';
+  audio.play();
+
+  setTimeout(() => {
+    award.classList.remove('animate')
+  }, 300);
+});
+
+game1.addEventListener('click', (e) => {
+  e.preventDefault();
+  game1.classList.add('animate');
+  audio.src = 'media/game1_music.mp3';
+  audio.play();
+
+  setTimeout(() => {
+    game1.classList.remove('animate')
+  }, 300);
+});
+
+game2.addEventListener('click', (e) => {
+  e.preventDefault();
+  game2.classList.add('animate');
+  audio.src = 'media/game2_music.mp3';
+  audio.play();
+
+  setTimeout(() => {
+    game2.classList.remove('animate')
+  }, 300);
+});
+
+game3.addEventListener('click', (e) => {
+  e.preventDefault();
+  game3.classList.add('animate');
+  audio.src = 'media/game3_music.mp3';
+  audio.play();
+
+  setTimeout(() => {
+    game3.classList.remove('animate')
+  }, 300);
+});
 
 speedInput.addEventListener('input', () => {
   audio.playbackRate = parseFloat(speedInput.value);
 });
 
-audio.addEventListener('ended', () => {
-  audio.currentTime = 0;
-  audio.play();
-});
-
-resetButton.addEventListener('click', () => {
+resetButton.addEventListener('click', (e) => {
+  e.preventDefault();
   audio.pause();
-  audio.currentTime = 0;
+  audio.src = '';
   audio.playbackRate = 1;
   speedInput.value = '1';
 });
